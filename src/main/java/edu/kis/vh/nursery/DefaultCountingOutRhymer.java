fbd63569621e5ceebe3476c7a35988a3f9898ace
Igor Jonski <71559210+IgorJonski@users.noreply.github.com>
2023-04-24 11:11:49 +0200

    Revert "Constants"

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,33 +2,32 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int SIZE = -1;
-    public static final int MAX_SIZE = 12;
+    private int[] numbers = new int[12];
 
-    final private int[] numbers = new int[MAX_SIZE];
-    public int total = SIZE;
+    public int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
+
     public boolean callCheck() {
-        return total == SIZE;
+        return total == -1;
     }
 
     public boolean isFull() {
-        return total == MAX_SIZE - 1;
+        return total == 11;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return SIZE;
+            return -1;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return SIZE;
+            return -1;
         return numbers[total--];
     }
 
